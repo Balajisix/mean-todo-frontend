a5238b6454bc229fd4ad30cabe416be8dcb6d174
Balaji <balajivs0305@gmail.com>
2025-04-05 21:17:24 +0530

    updates

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -19,22 +19,18 @@ export class TodoService {
   constructor(private http: HttpClient) { }
 
   getTodos(): Observable<Todo[]> {
-    console.log('Fetching from:', this.apiUrl);
     return this.http.get<Todo[]>(this.apiUrl);
   }
 
   addTodo(todo: Todo): Observable<Todo> {
-    console.log('Fetching from:', this.apiUrl);
     return this.http.post<Todo>(this.apiUrl, todo);
   }
 
   updateTodo(id: string, todo: Todo): Observable<Todo> {
-    console.log('Fetching from:', this.apiUrl);
     return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo);
   }
 
   deleteTodo(id: string): Observable<any> {
-    console.log('Fetching from:', this.apiUrl);
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
