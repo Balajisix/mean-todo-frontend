e2cb7fc07b91eedd6434ed66961a17e766d388b9
Balaji <balajivs0305@gmail.com>
2025-04-05 21:27:14 +0530

    updates

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-//
 export interface Todo {
   _id?: string;
   title: string;
@@ -14,23 +13,31 @@ export interface Todo {
 })
 export class TodoService {
   // private apiUrl = 'http://localhost:3000/api/todos';
-  private apiUrl = 'https://mean-todo-backend.vercel.app/api/todos'
+  private apiUrl = 'https://mean-todo-backend.vercel.app/api/todos';
 
   constructor(private http: HttpClient) { }
 
+  // Helper method to get authorization headers with token from localStorage
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      Authorization: token ? `Bearer ${token}` : ''
+    });
+  }
+
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.apiUrl);
+    return this.http.get<Todo[]>(this.apiUrl, { headers: this.getAuthHeaders() });
   }
 
   addTodo(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(this.apiUrl, todo);
+    return this.http.post<Todo>(this.apiUrl, todo, { headers: this.getAuthHeaders() });
   }
 
   updateTodo(id: string, todo: Todo): Observable<Todo> {
-    return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo);
+    return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo, { headers: this.getAuthHeaders() });
   }
 
   deleteTodo(id: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.getAuthHeaders() });
   }
 }
